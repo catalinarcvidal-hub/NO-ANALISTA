7f4745a318d18c7441352036176b3667814924f3
Catalina <catalinar.cvidal@gmail.com>
2026-04-11 16:11:07 -0400

    .

--- ejercicio_b_10_04.py
+++ ejercicio_b_10_04.py
@@ -1,6 +1,20 @@
 """
-Desarrolle un programa que permita ingrear un numero, si es
-negativo debemos enviar un mensaje 
-
+Desarrolle un programa que permita ingresar un número, si es
+negativo debemos enviar un mensaje al usuario, "Número inválido, 
+reingrese" y debe volver a ingresar otro número, si es positivo 
+enviamos un mensaje "Número correcto" y termina el programa.
 
 """
+num = 0
+num2 = 0
+
+num = int(input("Ingrese un número: "))
+
+if num > 0 :
+    print ("Número correcto.")
+else : 
+    print ("Número inválido, reingrese.")
+
+
+
+
