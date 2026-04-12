2860809f9697bab078ad591954c14eb1883366ff
Catalina <catalinar.cvidal@gmail.com>
2026-04-12 19:23:58 -0400

    .

--- clase_07_04.py/ejercicio3.py
+++ clase_07_04.py/ejercicio3.py
@@ -5,10 +5,40 @@
 a = 0
 b = 0
 c = 0
+pmayor = ""
+menor = ""
+medio = ""
 
-a = int(input("Ingrese el primer número"))
-b = int(input("Ingrese el segundo número"))
-c = int(input("Ingrese el tercer número"))
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+c = int(input("Ingrese el tercer número: "))
+
+
+if pmayor a > b :
+    a = "Posible mayor"
+    if mayor a > c :
+        a = "Mayor"
+        if medio b > c : 
+            b = "Medio"
+        else :
+            print("El valor medio es",medio,)
+
+else : 
+    b = "Posible mayor"
+    if b > c :
+        b = "Mayor"
+    if medio a > c : 
+            print("El valor medio es",a,)
+        else :
+            print("El valor medio es",c,)
+
+ 
+print ("Ordenamos los números ingresados de manera ascendente:" , menor, medio, mayor,".")
+
+
+
+    
+
 
 
 
--- clase_10_03.py
+++ clase_10_03.py
@@ -1,29 +1,35 @@
-#Apuntes primera clase de Introduccion a la programacion.
+""
+
+Apuntes primera clase de Introducción a la programacion.
 
 
-# Print es para mostrar informacion en la pantalla, todo lo que vaya despues de esta palabra se mostrara en la pantalla, considerando que el texto que queremos mostrar esta entre parentisis y comillas al inicio y al final.")
+Print es para mostrar informacion en la pantalla, todo lo que vaya despues de esta palabra se mostrara en la pantalla, considerando que el texto que queremos mostrar esta entre parentisis y comillas al inicio y al final.")
 
-#Ejemplo
+Ejemplo
 print("Hola mundo")
 print("Tu edad es:", 26)
 
-# Input pide datos al usuario desde el teclado, lo que el usuario ingrese se guardara en la variable que hayamos declarado para esto.       
+Input pide datos al usuario desde el teclado, lo que el usuario ingrese se guardara en la variable que hayamos declarado para esto.       
 
 edad = input("Ingresa tu edad: ")
 print("Tu edad es:", edad)
 
-#int Convierte un valor a número entero (sin decimales).
+int Convierte un valor a número entero (sin decimales).
 
 edad = int(input("¿Cuántos años tienes? "))
 print(edad + 1)
 
-#float Convierte un valor a número decimal.
+float Convierte un valor a número decimal.
 precio = float(input("¿Cuánto cuesta? "))
 print(precio * 2)
 
-#¿Por que combinarlos?
-#Porque input siempre devuelve texto, entonces si quieres hacer operaciones matemáticas necesitas convertirlo:
+¿Por que combinarlos?
+Porque input siempre devuelve texto, entonces si quieres hacer operaciones matemáticas
+necesitas convertirlo:
 numero = input("Ingresa un número: ")  # llega como texto "5"
 numero = int(numero)                   # ahora es el número 5
 print(numero * 2)                      # resultado: 10
+
+""
+
 
