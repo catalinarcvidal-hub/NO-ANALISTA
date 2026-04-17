984df2b349874e0f828926eab75c468a90ba4fbc
Catalina <catalinar.cvidal@gmail.com>
2026-04-17 19:41:24 -0400

    .

--- Introduccion_programacion_segura/Clase_15_04/ejercicio3.py
+++ Introduccion_programacion_segura/Clase_15_04/ejercicio3.py
@@ -3,17 +3,20 @@ Desarrolle un programa que permita ingresar mil numeros al usuario,
 al terminar despliegue la suma de ellos.
 
 """
-n_us = 0
+contador = 1
+numero = 0
 total = 0
-n_ing = 0
 
-n_us_in = int(input("Ingrese un número: "))
+while contador <= 3:
+    numero = int(input("Ingrese un número: "))
+    total = total + numero
+    print("Has ingresado", contador, "de 3.")
+    contador = contador + 1
+print("El total de la suma de los número ingresados es: ",total,)
 
 
-while n_ing <= 4:
-    print(n_ing)
-    n_us_in = int(input("Ingrese un número: "))
-    n_ing = n_ing + total
 
-total = n_us_in + n_us
-print (total)
+
+
+
+
