ef8662c73a697ce1cc7fc3a2c5c0e9081d8e93a3
Catalina <catalinar.cvidal@gmail.com>
2026-04-17 21:40:55 -0400

    .

---
+++ Introduccion_programacion_segura/Clase_15_04/ejerciciogpt.py
@@ -0,0 +1,25 @@
+"""
+Desarrolle un programa que permita ingresar cantidades de 
+platos consumidos por un cliente. El programa debe seguir 
+solicitando datos mientras el usuario lo desee. Al finalizar, 
+debe mostrar el total de platos consumidos.
+"""
+
+contador = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Introduccion_programacion_segura/Clase_17_04/ejercicio2.py
@@ -0,0 +1,8 @@
+"""
+Deben pedir al usuario hasta donde quiere 
+que llegue la serie de números.
+Si el usuario ingresa 10.
+"""
+serie = 0
+
+
