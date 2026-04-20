74fecbd89c07fdbc969fcce835e77f8289439197
catalinarcvidal-hub <catalinar.cvidal@gmail.com>
2026-04-20 13:26:44 -0400

    .

--- Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.2.py
+++ Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.2.py
@@ -6,17 +6,19 @@ en cuenta su sueldo base y comisiones.
 """
 
 sueldo_base = 0
-# Este intervalo representa el valor extra por venta.
+# Este intervalo representa la comision del 10% en decimal.
 comision = 10 / 100
-#Este intervalo es para representar la cantidad de ventas realizadas en el mes.
+#Este intervalo es para representar el monto total de ventas realizadas en el mes.
 ventas = 0
-#Este intervalo representa el porcentaje de comision que tendra por las ventas del mes
-tcxventas = 0
+#Este intervalo representa el monto de comision que tendra por las ventas del mes
+comision_total_ventas = 0
 
 total = 0
 
 sueldo_base = int(input("Ingrese el monto de su sueldo base: "))
-ventas = int(input("Ingrese la cantidad de ventas que realizo en el último mes: "))
+ventas = int(input("Ingrese el monto total de ventas que realizo en el último mes: "))
 
-tcxventas = comision * ventas3
-total = 
+comision_total_ventas = ventas * comision
+total = sueldo_base + comision_total_ventas
+
+print(f"El presente mes realizo un total de ${ventas} en ventas de las cuales recibira ${comision_total_ventas} por concepto de comisión. El total de su sueldo base mas las comisiones es de ${total} .")
