3ff9e38e57afa6e5a082e118f4afe8fae4fd152c
catalinarcvidal-hub <catalinar.cvidal@gmail.com>
2026-04-20 15:31:59 -0400

    Calculate final price after discount.

--- Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.3.py
+++ Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.3.py
@@ -1,1 +1,16 @@
+"""
+Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea 
+saber cuanto deberá pagar finalmente por su compra.
+"""
+descuento = 0
+precio = 0
 
+precio = int(input("Ingrese el valor del producto: "))
+descuento = int(input("Ingrese el porcentaje de descuento (Sin el signo %): "))
+
+#Calculo para convertir el descuento en decimal.
+descuento = descuento / 100
+#Calculo para restarle al precio original el descuento.
+precio_final = precio - (precio * descuento)
+
+print(f"El total a pagar es de ${precio_final}")
