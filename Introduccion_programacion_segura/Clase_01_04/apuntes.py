7a0cc7af388d2fb6b742c4cc060ef5d6d6336a36
Catalina <catalinar.cvidal@gmail.com>
2026-04-21 23:56:03 -0400

    ..

---
+++ Introduccion_programacion_segura/Clase_01_04/apuntes.py
@@ -0,0 +1,42 @@
+# Un SISTEMA: Tiene entrada, proceso y salida (O tambien determinada como Informacion)
+# Teoria general de sistemas(Libro) abarca conceptos como sinergia, entropia, etc. 
+# Teoria general de sistemas es que nosotros por ejemplo resolvemos miles y  miles de problemas 
+# en ell dia a dia.
+# Un sistema tiene infinitos subsistemas que trabajan en conjunto para funcionar.
+# Ejemplo: Entrada del sistema auditivo, sonido. Dispositivo de la entrada, oido.
+
+# TAREA: Encontrar 20 sistemas con entrada, proceso y salida. Y de cada uno debe tener 2 subsitemas 
+# minimo.
+
+# Ahora incluiremos condiciones(if) al programa. Como indicar si aprueba o no el ramo, la 
+# condicion seria que el promedio sea mayor a 4.0 por ejemplo. Las condiciones se traducen 
+# en verdadero o falso(solo dos caminos) .
+# Operadores relacionales son los que permiten comparar algo con algo, que es mayor, mayor igual,
+#  menor igual, igual y distinto. y estos operadores solo tienen la respuesta de si o no, si es mayor 
+# o no, si es menor o no, etc.
+
+#Operadores relacionales de java, python y otros.
+
+# > mayor q
+# >= mayor o igual
+# != distinto
+# == igual
+# < = menor o igual
+# < menor que
+
+##Diferencia de la utilizacion del =
+# == comparar
+# = operador de asigancion.
+
+#La condicion va abajo de la salida "Print".
+# if pro >= 4 :
+   # print("Aprobado")
+# else :
+   # print("Reprobado")
+#else significa si no, o sea que si no se cumple la condicion, se dirige a ese camino.
+
+#print(Fin del programa)
+
+#Cuando uno pone algo hacia adentro se llama identacion y se hace con el tabulador o triple espacio.
+
+#Ver Silicon Valley (Serie ñoña)
