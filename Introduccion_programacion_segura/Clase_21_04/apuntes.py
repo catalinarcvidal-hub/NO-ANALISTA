646facfd69b36503d86e2bd4017dc98b3fc28418
Catalina <catalinar.cvidal@gmail.com>
2026-04-22 14:45:37 -0400

    .

---
+++ Introduccion_programacion_segura/Clase_21_04/apuntes.py
@@ -0,0 +1,11 @@
+"""
+Regla de prioridades.
+
+not - and - or
+
+Operador nuevo.
+
+// Division que trunca solo el resultado entero. No se considera los numeros despues de la coma.
+5//2 = 2
+
+"""
---
+++ Introduccion_programacion_segura/Guia de ejercicios/ejercicio3.1.py
@@ -0,0 +1,23 @@
+"""
+Calcular el promedio de un alumno que tiene 7 calificaciones en la materia de Diseño Estructurado de Algoritmos 
+"""
+nota1 = 0.0
+nota = 0.0
+promedio = 0.0
+suma_notas = 0.0
+contador = 1
+ciclos = 7
+
+print("Calculadora de promedio")
+print("Ingresa hasta 7 notas y calcularemos tu promedio.")
+print()
+
+while contador <= ciclos :
+    nota = float(input("Ingrese la nota: "))
+    print(f"Has ingresado {contador} de {ciclos}.")
+  
+    suma_notas = ( suma_notas + nota ) 
+    contador = contador + 1
+    
+promedio = suma_notas / 7
+print(f"Su promedio es {promedio}")
