2155f867c7e77617f7a6810a8435b507d90742e8
Catalina <catalinar.cvidal@gmail.com>
2026-04-21 23:53:17 -0400

    recuperando

--- Introduccion_programacion_segura/Clase_01_04/tarea.py
+++
@@ -1,8 +0,0 @@
-#Entrada: Netflix
-#Proceso: Registrarse - Ingresar metodo de pago 
-#Salida: Tener acceso a peliculas y series.
-
-#Subsitema 1: Dinero.
-#Subsitema 2: Contar con un dispositivo .
-
-
