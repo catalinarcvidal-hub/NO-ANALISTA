34735f88cf6a34b83c03b375d12c38a93cb6066f
Catalina <catalinar.cvidal@gmail.com>
2026-04-23 23:21:39 -0400

    .

--- Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.1.py
+++ Introduccion_programacion_segura/Guia de ejercicios/ejercicio2.1.py
@@ -4,25 +4,20 @@ cuanto dinero ganara después de un mes si el banco paga a razón de 2% mensual.
 """
 
 capital = 0
-#tasa de interes de un 2% que pasare a decimal -> 2 / 100 -> 0.02
 tasa_mensual = 0.0
 
-
+tasa_mensual = float(input("Ingrese el porcentaje de la tasa mensual: "))
 capital = int(input("Ingrese el monto del capital inicial: "))
-tasa = float(input("Ingrese la tasa mensual: "))
 
-tasa = tasa / 100
-
-if capital > 0:
-    
-    
-
-
-
-ganancia = capital * tasa_mensual
-
-print(f"Al cabo de un mes con una tasa de interes del 2% tendra una ganancia de ${ganancia}.")
+if tasa_mensual > 0 and capital > 0:
+    #tasa de interes se pasa a decimal ej: 2% -> 2 / 100 -> 0.02
+    tasa_mensual = tasa_mensual / 100
+    ganancia = capital * tasa_mensual
+    total = capital + ganancia
+    print(f"Al cabo de un mes con una tasa de interés del {tasa_mensual}% tendra una ganancia de ${ganancia} y un total de ${total}")
+else:
+    print("El monto del capital y la tasa mensual deben ser mayor a 0.")
 
 
 
-"""
+
