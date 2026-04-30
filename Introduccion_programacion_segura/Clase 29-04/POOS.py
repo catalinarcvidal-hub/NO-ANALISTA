cb6063587524e1857943825a063a768ba6e3e09b
Catalina <catalinar.cvidal@gmail.com>
2026-04-29 21:47:18 -0400

    .

---
+++ Introduccion_programacion_segura/Clase 29-04/POOS.py
@@ -0,0 +1,23 @@
+"""
+¿Que es un paradigma?
+objetos tienen atributos o propiedades (son caracterisiticas del objeto, ejemplo seres humanos
+tenemos estatura, peso, rut, etc) y los metodos (Son las acciones que tiene el 
+objeto, nosotros como seres humanos tenemos las acciones de caminar, hablar, razonar,etc)
+
+Debemos establecer atributos y acciones del objeto.
+
+
+Debemos identificar objetos tangibles (Auto) e intangibles (nombre)
+
+La clase es un molde y en esa clase es cuando identificar los atributos 
+
+
+TAREA
+
+Junto con la investigacion deben identificar de su vida cotidiana 10 objetos y por cada objeto al menos 
+5 atributos y 3 metodos.
+
+Atributos prohibidos. Color, marca, nombre, modelo, apellido.
+Prohibido usar los ejemplos.
+
+"""
