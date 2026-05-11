eb1715ccae065de94a8c77883736677e16e970f1
catalinarcvidal-hub <catalinar.cvidal@gmail.com>
2026-05-11 19:08:40 -0400

    Actualizar e.numeros_pares.py

--- Introduccion_programacion_segura/Clase 08-05/e.numeros_pares.py
+++ Introduccion_programacion_segura/Clase 08-05/e.numeros_pares.py
@@ -2,18 +2,10 @@
 Programa para determinar si un número es o no par.
 """
 
-# Inicializamos un contador para llevar el registro de cuántos divisores exactos tiene el número
-divisibles = 0
+def es_par(numero):
+    if numero % 2 == 0:
+        return True
+    else: 
+        return False
 
-# Iniciamos un ciclo que recorre desde 1 hasta el valor de 'numero' inclusive
-for x in range(1, numero + 1):
-    # Verificamos si el resto de la división (módulo) es cero
-    if numero % x == 0:
-        # Si es divisible, incrementamos nuestro contador en 1
-        divisibles = divisibles + 1
-
-# Un número es primo si y solo si tiene exactamente dos divisores: 1 y él mismo
-if divisibles == 2:
-    return True  # Es primo
-else:
-    return False # No es primo
+print(es_par(7362))
