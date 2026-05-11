616330a151b6a9a3a112f63eb2460002a84df10a
catalinarcvidal-hub <catalinar.cvidal@gmail.com>
2026-05-11 17:59:01 -0400

    probar si esta bien.

--- Introduccion_programacion_segura/Clase 08-05/e.funcion_numero_amigos.py
+++ Introduccion_programacion_segura/Clase 08-05/e.funcion_numero_amigos.py
@@ -8,7 +8,27 @@ a = 0
 b = 0
 
 def numeros_amigos(a,b):
-  a = 
+  a = 0
+  b = 0
+  suma = 0
+
+  for x in range(1,a):
+    if a % x == 0:
+      suma_a = suma_a + x
+
+  for x in range(1,b):
+    if b % x == 0:
+      suma_b = suma_b + x
+
+  if suma_a == b and suma_b == a:
+    print(f"{a} y {b} son números amigos.")
+
+
+  
+      
+
+
+
   
   
   
