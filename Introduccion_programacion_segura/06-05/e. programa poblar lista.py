fbcc267097fd74c0e9961031e150b6a986583738
catalinarcvidal-hub <catalinar.cvidal@gmail.com>
2026-05-12 11:21:32 -0400

    cambio
    Refactor list population to use a for loop instead of a while loop.

--- Introduccion_programacion_segura/06-05/e. programa poblar lista.py
+++ Introduccion_programacion_segura/06-05/e. programa poblar lista.py
@@ -1,10 +1,12 @@
 """
 Desarrolle un programa que permita poblar una lista de tamaño 50 (o sea 49 elementos)
 """
-c = 0
-e = 0
-
 numeros = []
 
-while c < 50:
-    #
+#La x es la variable que es el contador y llegara hasta el 49 ya que el numero que se pone en el parentesis es excluyente, no se considera.
+for x in range(1,50):
+    #Ponemos el nombre de la lista un punto y append para agregar elementos a la lista.
+    numeros.append(x)
+#Con este print, me mostrara la lista poblada del 1 al 49 consecutivamente.
+print(numeros)
+    
